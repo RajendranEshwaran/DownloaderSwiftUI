74e15717173772d40bfeb029901f946d7467ec18
RajendranEshwaran <rajendranmca2010@gmail.com>
2022-01-19 12:45:06 -0500

    Initial Commit

---
+++ Shared/DownloaderSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DownloaderSwiftUIApp.swift
+//  Shared
+//
+//  Created by gomathi saminathan on 1/19/22.
+//
+
+import SwiftUI
+
+@main
+struct DownloaderSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
